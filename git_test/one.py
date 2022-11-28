2d8d553573fdbdb0a98d4b27fadd5e1182b10e94
198192baicun <1981927515@qq.com>
2022-11-28 22:22:05 +0800

    第五次提交，sys_get

--- git_test/one.py
+++ git_test/one.py
@@ -3,13 +3,18 @@
 # 日期：2022年11月28日
 
 import requests
+import sys
+
+def sys_get():
+    print(sys.argv)
 
 def mast():
     a = 5
     b = 5
-    print(a+b)
-    response = requests.get("www.baidu.com")
+    print(a + b)
+    response = requests.get("https://www.baidu.com")
     print(response.text)
+
 
 if __name__ == '__main__':
     print("hello world!")
