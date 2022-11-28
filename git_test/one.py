13bec677e062bc0b3013614217073ff99ce39b19
198192baicun <1981927515@qq.com>
2022-11-28 22:02:42 +0800

    第四次提交，mast函数，百度response

--- git_test/one.py
+++ git_test/one.py
@@ -2,10 +2,14 @@
 # author：LJ
 # 日期：2022年11月28日
 
+import requests
+
 def mast():
     a = 5
     b = 5
     print(a+b)
+    response = requests.get("www.baidu.com")
+    print(response.text)
 
 if __name__ == '__main__':
     print("hello world!")
